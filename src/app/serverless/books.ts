6487f943f093f30b3a13b91d660a89151ef46e9e
Vishnu <vishnu@propelinc.com>
2023-07-05 11:52:55 +0530

    third

--- src/app/serverless/books.ts
+++ src/app/serverless/books.ts
@@ -1,22 +1,22 @@
-import { Client } from 'pg';
-import * as dotenv from 'dotenv';
-dotenv.config();
-export default async function handler(event: any, context: any) {
-  const client = new Client({
-    host: process.env['VERCEL_DATABASE_HOST'],
-    port: parseInt(process.env['VERCEL_DATABASE_PORT']!),
-    database: process.env['VERCEL_DATABASE_NAME'],
-    user: process.env['VERCEL_DATABASE_USER'],
-    password: process.env['VERCEL_DATABASE_PASSWORD'],
-  });
-  const result = await client.query(`SELECT * FROM Books;`);
-  const booksData = result.rows.map(
-    ({ title, author, description, image }) => ({
-      title,
-      author,
-      description,
-      image,
-    })
-  );
-  context.res.status(200).json(booksData);
-}
+// import { Client } from 'pg';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+// export default async function handler(event: any, context: any) {
+//   const client = new Client({
+//     host: process.env['VERCEL_DATABASE_HOST'],
+//     port: parseInt(process.env['VERCEL_DATABASE_PORT']!),
+//     database: process.env['VERCEL_DATABASE_NAME'],
+//     user: process.env['VERCEL_DATABASE_USER'],
+//     password: process.env['VERCEL_DATABASE_PASSWORD'],
+//   });
+//   const result = await client.query(`SELECT * FROM Books;`);
+//   const booksData = result.rows.map(
+//     ({ title, author, description, image }) => ({
+//       title,
+//       author,
+//       description,
+//       image,
+//     })
+//   );
+//   context.res.status(200).json(booksData);
+// }
