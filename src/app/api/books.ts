79bfcb51de879d245572efa3e6b023dbf5ca55c2
Vishnu <vishnu@propelinc.com>
2023-07-05 11:33:36 +0530

    second

---
+++ src/app/api/books.ts
@@ -0,0 +1,25 @@
+import { Client } from 'pg';
+import * as dotenv from 'dotenv';
+import { db } from '@vercel/postgres';
+
+dotenv.config();
+export default async function handler(event: any, context: any) {
+  const client = await db.connect();
+  // const client = new Client({
+  //   host: process.env['VERCEL_DATABASE_HOST'],
+  //   port: parseInt(process.env['VERCEL_DATABASE_PORT']!),
+  //   database: process.env['VERCEL_DATABASE_NAME'],
+  //   user: process.env['VERCEL_DATABASE_USER'],
+  //   password: process.env['VERCEL_DATABASE_PASSWORD'],
+  // });
+  const result = await client.query(`SELECT * FROM Books;`);
+  const booksData = result.rows.map(
+    ({ title, author, description, image }) => ({
+      title,
+      author,
+      description,
+      image,
+    })
+  );
+  context.res.status(200).json(booksData);
+}
